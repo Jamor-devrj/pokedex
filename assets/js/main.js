6e8375351be3d7a531414699685384ca1ed936ac
jamor <jamor.devbr@gmail.com>
2024-01-15 20:47:05 -0300

    Primeirora requisição

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,11 @@
 
-console.log('Sucess!')
+const offset = 0
+const limit = 10
+const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+
+fetch(url).then(function (reponse) {
+    console.log(reponse)
+})
+
+const x = 10 + 10;
+console.log(x)

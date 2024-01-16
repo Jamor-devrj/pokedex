e606edc7ae278c74bcc12885be7f242b4b3d69ff
jamor <jamor.devbr@gmail.com>
2024-01-16 14:04:47 -0300

    Map para menos verbosidade do  Código (seguindo curso, falta algo no codigo)

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,5 @@
 
-const offset = 0
-const limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-
-function convertPokemonToLi(pokemon){
+function convertPokemonToLi(pokemon){ //para criar elementos da lista usando tags do json. A mesma função vai se chamada no fetch
     return `
         <li class="pokemon">
                     <span class="number">#001</span>
@@ -20,17 +16,8 @@ function convertPokemonToLi(pokemon){
     `
 }
 
-const pokemonList = document.getElementById('pokemonList')
+const pokemonList = document.getElementById('pokemonList') 
 
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons) => {
-
-        for (let i = 0; i < pokemons.length; i++) {
-            const pokemon = pokemons[i];            
-            pokemonList.innerHTML += convertPokemonToLi(pokemon)
-        }  
-    })
-    .catch((error) => console.error(error))
-  
+pokeApi.getPokemons().then((pokemons = []) => {
+    pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join('')
+})
